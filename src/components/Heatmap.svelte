a3afcaa421501d113a8175e2ac151c37f0f7da64
76tebe <tubagusghiffarimaulana@gmail.com>
2025-10-13 20:36:15 +0700

    feat: add heatmap visualization for Strava activities

--- src/components/Heatmap.svelte
+++ src/components/Heatmap.svelte
@@ -1,12 +1,71 @@
 <script>
   import CalHeatmap from "cal-heatmap";
+  import Tooltip from "cal-heatmap/plugins/Tooltip";
   import "cal-heatmap/cal-heatmap.css";
+  import { onMount } from "svelte";
 
   const cal = new CalHeatmap();
-  cal.paint({
 
-  });  // const paint = `<div id="cal-heatmap"></div>`;
+  onMount(async () => {
+    const response = await fetch("/api/activities");
+    const json = await response.json();
+
+    const data = json.activities.map((a) => ({
+      start_date: a.start_date,
+      moving_time: a.moving_time / 60,
+    }));
+
+    cal.paint(
+      {
+        data: {
+          source: data,
+          type: "json",
+          x: "start_date",
+          y: "moving_time",
+        },
+        date: {
+          start: new Date("2025-01-01"),
+        },
+        scale: {
+          color: {
+            type: "quantize",
+            scheme: "Greens",
+            domain: [0, 20, 40, 60, 80, 100],
+          },
+        },
+        domain: {
+          type: "month",
+          gutter: 4,
+          label: {
+            text: "MMM",
+            textAlign: "start",
+            position: "top",
+          },
+        },
+        subDomain: {
+          type: "ghDay",
+          radius: 2,
+          width: 11,
+          height: 11,
+          gutter: 4,
+        },
+      },
+      [
+        [
+          Tooltip,
+          {
+            text: function (date, value, dayjsDate) {
+              return (
+                (value ? value : "0") +
+                " minutes on " +
+                dayjsDate.format("dddd, MMMM D, YYYY")
+              );
+            },
+          },
+        ],
+      ]
+    );
+  });
 </script>
 
 <div id="cal-heatmap"></div>
-
