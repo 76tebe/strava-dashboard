f23f3c1bedc3d4fc5c93629cdd0cbc9ab8d85170
76tebe <tubagusghiffarimaulana@gmail.com>
2025-10-30 17:47:44 +0700

    feat: add dock menu

---
+++ src/components/Dock/Dock.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { Github, Linkedin } from "lucide-svelte";
+  import Ava from "../../assets/tebe-profile-transparent.png";
+  import DockMenu from "./DockMenu.svelte";
+  import DockItem from "./DockItem.svelte";
+  let navs = [
+    { label: "Linkedin", icon: Linkedin, href: "https://www.linkedin.com/in/tubagus-ghiffari-maulana/" },
+    { label: "Github", icon: Github, href: "https://github.com/76tebe" },
+  ];
+  import * as Tooltip from "$lib/components/ui/tooltip";
+</script>
+
+<DockMenu direction="middle" let:mouseX let:distance let:magnification>
+  <DockItem {mouseX} {magnification} {distance}>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger
+          class="hover:bg-slate-900/10 transition-all duration-200 rounded-full"
+        >
+        <a href="https://tebe.world" target="_blank">
+          <img
+          src={Ava.src}
+              alt="Portfolio Tebe"
+              class="m-3 h-6 w-6"
+            />
+        </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content sideOffset={8}>
+          <p>Portfolio Tebe</p>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  </DockItem>
+  {#each navs as item}
+    <DockItem {mouseX} {magnification} {distance}>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            class="hover:bg-slate-900/10 transition-all duration-200 rounded-full p-3"
+          >
+          <a href={item.href} target="_blank">
+            <svelte:component this={item.icon} size={20} strokeWidth={1.2} />
+          </a>
+          </Tooltip.Trigger>
+          <Tooltip.Content sideOffset={8}>
+            <p>{item.label}</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    </DockItem>
+  {/each}
+</DockMenu>
